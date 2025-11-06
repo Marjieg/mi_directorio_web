51a604eee8f0a310fc66695880dd70929e102455
Marjieg <marinajegea@gmail.com>
2025-11-06 20:05:15 +0100

    bucles for

--- aprendo/nivel1.php
+++ aprendo/nivel1.php
@@ -20,6 +20,53 @@ if ($numero >= 0){
 for($i=1;$i<=10;$i++){
     echo "contando $i <br>";
 }
+//El siguiente ejemplo muestra los números del 10 al 1.
+
+for ($i=10; $i >= 1; $i--){
+    echo $i;
+    echo "<br>";
+}
+//Escribe un script PHP que muestre los números del 1 al 10 en una tabla 
+//de una fila y 10 columnas. Utiliza un bucle for
+echo "<table border='1'><tr>";
+for($i=1;$i<=10;$i++){
+    echo "<td>$i</td>";
+}
+echo "</tr></table><br>";
+//Escribe un script PHP que muestre los números del 1 al 10 en una tabla 
+//de una columna y 10 filas. Utiliza un bucle for.tr filas, td columnas
+
+echo "<table border='1'><tr>";
+for($i=1;$i<=10;$i++){
+    echo "<tr><td>$i</td></tr>";
+    
+}
+echo "</table>";
+
+//Escribe un script PHP que muestre en una tabla los números pares que 
+// existen entre 1 y 100. Utiliza un bucle for.
+
+for($i=1;$i <=100;$i++){
+    if($i % 2 == 0){
+        echo "$i,";
+    }
+}
+//Escribe un script PHP que muestre la tabla de multiplicar de un número 
+// aleatorio. Utiliza un bucle for
+$num = rand(1,10);
+echo $num;
+for($i=0;$i<=10;$i++){
+    $multi = $num * $i;
+    echo "$num * $i = $multi<br>";
+}
+//Escribe un script PHP que muestre las tablas de multiplicar del 1 al 10. Utiliza un bucle for
+
+for($i=1;$i<=10;$i++){
+  for($j=1;$j<=10;$j++){
+    $multi = $i * $j;
+    echo "$i x $j = $multi<br>";
+  }
+}
     ?>
     
 </body>
--- aprendo/nivel11.php
+++ aprendo/nivel11.php
@@ -7,11 +7,41 @@ $moneda = rand(1,2);
 if($moneda == 1){
     $moneda = "cara";
     echo "<img src= 'cara.jpg' width= '80' heigth= '80'/>";
-    echo "ha salido $moneda";
+    echo "ha salido $moneda<br>";
 }else{
     $moneda = "cruz";
     echo "<img src= 'cruz.jpg' width= '80' heigth= '80'/>";
-    echo "ha salido $moneda";
+    echo "ha salido $moneda <br> ";
 }
+
+/*Escribe un script PHP que genere un número aleatorio entre 1 y 7, y muestre 
+un mensaje indicando a qué día de la semana corresponde. Por ejemplo, 1 sería lunes, 2 martes, etc.*/
+
+$num = rand(1,7);
+switch($num){
+    case 1:
+        echo "Es lunes";
+        break;
+    case 2:
+        echo "Es martes";
+        break;
+    case 3:
+        echo "Es miercoles";
+        break;
+    case 4:
+        echo "Es jueves";
+        break;
+    case 5:
+        echo "Es viernes";
+        break;
+    case 6;
+        echo "Es sábado";
+        break;
+    case 7;
+        echo "Es domingo"; 
+        break;   
+}
+
+
 
 ?>
